a63092db0072ce71e758ebbb2cd46a12f4e8bf7d
Victor <victorricarte155@gmail.com>
2024-04-30 18:16:52 -0300

    +2 exercicios resolvidos

--- Iniciante - Pag3/exc1074.cs
+++ Iniciante - Pag3/exc1074.cs
@@ -2,7 +2,16 @@ internal class beecrowd
 {
     private static void Main(string[] args)
     {
-
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            if(x % 2 == 0 && x > 0) { Console.WriteLine("EVEN POSITIVE"); }
+            if(x % 2 == 0 && x < 0) { Console.WriteLine("EVEN NEGATIVE"); }
+            if(x % 2 != 0 && x > 0) { Console.WriteLine("ODD POSITIVE"); }
+            if(x % 2 != 0 && x < 0) { Console.WriteLine("ODD NEGATIVE"); }
+            if(x == 0) { Console.WriteLine("NULL"); }
+        }
     }
 
 }
--- Iniciante - Pag3/exc1075.cs
+++ Iniciante - Pag3/exc1075.cs
@@ -2,7 +2,11 @@ internal class beecrowd
 {
     private static void Main(string[] args)
     {
-
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= 10000; i++)
+        {
+            if(i % n == 2) { Console.WriteLine(i); }
+        }
     }
 
 }
